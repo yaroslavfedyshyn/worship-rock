0a59e38cd9c48b6c63d2d17aa96dcde6b709f16d
korin <korin.yaroslav@gmail.com>
2019-09-18 17:05:19 +0300

    WR-13 Connect design

--- src/containers/MainCover/components/MainCover.js
+++ src/containers/MainCover/components/MainCover.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import MainCoverImg from '../../../../src/img/bg-img/bg-2.jpg'
+import MainCoverImg from '../../../../src/img/bg-img/blog1.jpg'
 
 const MainCover = (props) => {
 
@@ -11,29 +11,6 @@ const MainCover = (props) => {
     return (
         <section className="hero-area">
             <div className="hero-slides owl-carousel">
-
-                { /* Single Hero Slide */}
-
-                <div className="single-hero-slide d-flex align-items-center justify-content-center">
-                    {/* Slide Img */}
-                    <div className="slide-img bg-img"></div>
-                    {/* style="background-image: url(img/bg-img/bg-1.jpg);" */}
-                    {/* Slide Content */}
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="hero-slides-content text-center">
-                                    <h6 data-animation="fadeInUp" data-delay="100ms">Latest album</h6>
-                                    <h2 data-animation="fadeInUp" data-delay="300ms">Beyond Time <span>Beyond Time</span>
-                                    </h2>
-                                    <a data-animation="fadeInUp" data-delay="500ms" href="#"
-                                       className="btn oneMusic-btn mt-50">Discover <i
-                                        className="fa fa-angle-double-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 {/* Single Hero Slide */}
                 <div className="single-hero-slide d-flex align-items-center justify-content-center">
@@ -46,7 +23,7 @@ const MainCover = (props) => {
                             <div className="col-12">
                                 <div className="hero-slides-content text-center">
                                     <h6 data-animation="fadeInUp" data-delay="100ms">Latest album</h6>
-                                    <h2 data-animation="fadeInUp" data-delay="300ms">Colorlib Music <span>Colorlib Music</span>
+                                    <h2 data-animation="fadeInUp" data-delay="300ms">Worship Music <span>Worship Musicc</span>
                                     </h2>
                                     <a data-animation="fadeInUp" data-delay="500ms" href="#"
                                        className="btn oneMusic-btn mt-50">Discover <i
